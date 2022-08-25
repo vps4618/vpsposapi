9dbc3ee6a50fe2a4db5931cb6ecb9d4fc7bc836a
vps4618 <vishwapraveen4618@gmail.com>
2022-08-26 00:59:53 +0530

    origin updated

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors({
 }))
 
 app.get('/',(req,res) => {
-    res.send('Welcome to VpsPosApi . Created by VPS . ');
+    res.send('Welcome to VpsPosApi. Created by VPS. ');
 })
 
 //get all data
