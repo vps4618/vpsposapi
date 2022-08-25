b259444ecbcd512ccda09e027b045f468a1c55a4
vps4618 <vishwapraveen4618@gmail.com>
2022-08-26 00:41:08 +0530

    origin updated

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-    origin:"https://vps4618.github.com/",
+    origin:"https://vps4618.github.io/",
     methods:["GET"]
 }))
 
